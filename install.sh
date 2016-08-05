6d3d206fe8b5ab4a33b90be245da9f24071ec46e
sanraizuizumo <sanraizuizumo@gmail.com>
2016-08-05 22:01:53 +0900

    未完

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 
 # dotfiles/install.sh
 
-ln -si .vimrc ~/.vimrc
-ln -si .zshrc ~/.zshrc
-ln -si .tmux.conf ~/.tmux.conf
+ln -si dotfiles/.vimrc ~/.vimrc
+ln -si dotfiles/.zshrc ~/.zshrc
+ln -si dotfiles/.tmux.conf ~/.tmux.conf
