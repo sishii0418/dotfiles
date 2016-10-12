14f970ee1d689129d1af7c921afaffc682962d2b
sanraizuizumo <sanraizuizumo@gmail.com>
2016-10-12 17:24:00 +0900

    commited

--- install.sh
+++ install.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-
 # dotfiles/install.sh
 
 ln -si dotfiles/.vimrc ~/.vimrc
-ln -si dotfiles/dein.toml ~/.vim/rc/dein.toml
 ln -si dotfiles/.zshrc ~/.zshrc
 ln -si dotfiles/.tmux.conf ~/.tmux.conf
