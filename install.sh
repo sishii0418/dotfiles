38c6bc6cc87550587c3d6f418a9136833e73bd68
sanraizuizumo <sanraizuizumo@gmail.com>
2016-11-06 14:25:45 +0900

    for nvim

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 # dotfiles/install.sh
 
-ln -si dotfiles/.vimrc ~/.vimrc
+if [! -f ~/.config/nvim]; then
+  mkdir ~/.config/nvim
+fi
+
+ln -si dotfiles/init.vim ~/.config/nvim/init.vim
 ln -si dotfiles/.zshrc ~/.zshrc
 ln -si dotfiles/.tmux.conf ~/.tmux.conf
 ln -si dotfiles/.Xresources ~/.Xresources
