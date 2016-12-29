1eba988cb5b7bff6a6c3e7378ce378c2f047af39
sanraizuizumo <sanraizuizumo@gmail.com>
2016-12-29 17:21:35 +0900

    added dein*

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 # dotfiles/install.sh
 
 ln -si dotfiles/init.vim ~/.config/nvim/init.vim
+ln -si dotfiles/dein.toml ~/.config/nvim/dein.toml
+ln -si dotfiles/deinlazy.toml ~/.config/nvim/deinlazy.toml
 ln -si dotfiles/.zshrc ~/.zshrc
 ln -si dotfiles/.tmux.conf ~/.tmux.conf
 ln -si dotfiles/.Xresources ~/.Xresources
