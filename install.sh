fb8c4ada1b9f57586979957350ead2864c7f3a56
sanraizuizumo <sanraizuizumo@gmail.com>
2017-02-19 14:51:42 +0900

    fixed

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # dotfiles/install.sh
 
-# NOTE: run at ~/dotfiles
-ln -si ./init.vim       $XDG_CONFIG_HOME/nvim/init.vim
-ln -si ./dein.toml      $XDG_CONFIG_HOME/nvim/dein.toml
-ln -si ./deinlazy.toml  $XDG_CONFIG_HOME/nvim/deinlazy.toml
-ln -si ./.zshrc         ~/.zshrc
-ln -si ./.tmux.conf     ~/.tmux.conf
-ln -si ./.Xresources    ~/.Xresources
+ln -si $HOME/dotfiles/init.vim      $XDG_CONFIG_HOME/nvim/init.vim
+ln -si $HOME/dotfiles/dein.toml     $XDG_CONFIG_HOME/nvim/dein.toml
+ln -si $HOME/dotfiles/deinlazy.toml $XDG_CONFIG_HOME/nvim/deinlazy.toml
+ln -si $HOME/dotfiles/.zshrc        $HOME/.zshrc
+ln -si $HOME/dotfiles/.tmux.conf    $HOME/.tmux.conf
+ln -si $HOME/dotfiles/.Xresources   $HOME/.Xresources
+ln -si $HOME/dotfiles/.xinitrc      $HOME/.xinitrc
