ee33fae5821e4354dc0f0234a77f5ea7b62ca9f2
sishii0418 <s.ishii@sms.ed.ac.uk>
2022-06-25 10:50:22 +0900

    added XDG_CONFIG_HOME and keymapsForVSCode.vim

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # dotfiles/install.sh
+export XDG_CONFIG_HOME=$HOME/.config
 
 ln -si $HOME/dotfiles/init.vim      $XDG_CONFIG_HOME/nvim/init.vim
 ln -si $HOME/dotfiles/dein.toml     $XDG_CONFIG_HOME/nvim/dein.toml
@@ -9,3 +10,4 @@ ln -si $HOME/dotfiles/.tmux.conf    $HOME/.tmux.conf
 ln -si $HOME/dotfiles/.Xresources   $HOME/.Xresources
 ln -si $HOME/dotfiles/.xinitrc      $HOME/.xinitrc
 ln -si $HOME/dotfiles/.latexmkrc    $HOME/.latexmkrc
+ln -si $HOME/dotfiles/keymapsForVSCode.vim $XDG_CONFIG_HOME/Code/User/keymapsForVSCode.vim
